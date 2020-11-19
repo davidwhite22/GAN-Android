fbae5eb8606bfb84145be8b88461fd1090090838
David White <gomobile.dw@gmail.com>
2020-11-19 18:53:23 +0000

    Removed unused static method

--- app/src/main/java/com/gomobi/breakingbad/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gomobi/breakingbad/ui/home/HomeFragment.kt
@@ -121,14 +121,6 @@ class HomeFragment: BaseFragment() {
 
     companion object {
         private const val MAXSERIES = 5
-        //region Keyboard Stuff
-        fun dismissKeyboard(view: View?) {
-            view?.let {
-                val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
-            }
-        }
-        //endregion
     }
 
 }
